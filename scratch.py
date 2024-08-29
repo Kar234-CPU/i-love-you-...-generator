36f06c0ff3a27a8cd1c7f08b1fd3ad6fd95ce04c
Kar234-CPU <145925597+Kar234-CPU@users.noreply.github.com>
2024-08-29 15:36:02 +0200

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,21 @@
+#this was made by @Kar234-CPU
+#hope this helps you
+#also as you can see this is a easy scrip
+
+import time
+
+print(' ')
+print("this is a i love you ...% generator")
+print('you need to copy this and paste it somewhere you need it')
+print('"1.word" ...% "2.word"')
+print(' ')
+time.sleep(1)
+first_word = input("What is your first word: ")
+second_word = input("What is your second word: ")
+print('//////////////////////////////////////////////////////////////////////////////////////////////////')
+i = 1
+while i <= 100:
+    print(first_word, i, '%', second_word)
+    i = i + 1
+    time.sleep(0.01)
+print('//////////////////////////////////////////////////////////////////////////////////////////////////')
